943a90f7a00aac8024e658d6751b0db3c15ea6bd
Edilson Borges <edilson@edilsonborges.com.br>
2017-09-06 20:03:51 -0300

    save and delet method for album

--- module/Album/src/Album/Model/AlbumTable.php
+++ module/Album/src/Album/Model/AlbumTable.php
@@ -29,4 +29,27 @@
          return $row;
      }
 
+     public function saveAlbum(Album $album)
+     {
+         $data = array(
+             'artist' => $album->artist,
+             'title'  => $album->title,
+         );
+
+         $id = (int) $album->id;
+         if ($id == 0) {
+             $this->tableGateway->insert($data);
+         } else {
+             if ($this->getAlbum($id)) {
+                 $this->tableGateway->update($data, array('id' => $id));
+             } else {
+                 throw new \Exception('Album id does not exist');
+             }
+         }
+     }
+
+     public function deleteAlbum($id)
+     {
+         $this->tableGateway->delete(array('id' => (int) $id));
+     }
  }
