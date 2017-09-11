073429c257a7d28670ae9567edd8b864e88d92ea
Edilson Borges <edilsonfb@gmail.com>
2017-09-11 01:44:39 -0300

    Edit model, action and form

--- module/Album/src/Album/Model/Album.php
+++ module/Album/src/Album/Model/Album.php
@@ -18,6 +18,11 @@ class Album implements InputFilterAwareInterface
         $this->artist = (isset($data['artist'])) ? $data['artist'] : null;
         $this->title  = (isset($data['title']))  ? $data['title']  : null;
     }
+    
+    public function getArrayCopy()
+    {
+        return get_object_vars($this);
+    }
 
     // Add content to these methods:
     public function setInputFilter(InputFilterInterface $inputFilter)
