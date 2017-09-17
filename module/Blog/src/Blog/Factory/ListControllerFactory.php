bd8cae6e64657640e2681d7a4ca4147b7e3074b1
Edilson Borges <edilsonfb@gmail.com>
2017-09-17 19:20:25 -0300

    function createService

--- module/Blog/src/Blog/Factory/ListControllerFactory.php
+++ module/Blog/src/Blog/Factory/ListControllerFactory.php
@@ -8,5 +8,11 @@ use Zend\ServiceManager\ServiceLocatorInterface;
 
 class ListControllerFactory implements FactoryInterface
 {
-	
+	public function createService(ServiceLocatorInterface $serviceLocator)
+	{
+		$realServiceLocator = $serviceLocator->getServiceLocator();
+		$postService = $realServiceLocator->get('Blog\Service\PostServiceInterface');
+
+		return new ListController($postService);
+	}
 }
