63f61be0533ce2c8e84ad4138e2a41207baa7372
Edilson Borges <edilsonfb@gmail.com>
2017-09-17 19:18:08 -0300

    Factory class

---
+++ module/Blog/src/Blog/Factory/ListControllerFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Blog\Factory;
+
+use Blog\Controller\ListController;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class ListControllerFactory implements FactoryInterface
+{
+	
+}
