068acded95fff2e8e6a0f5fbe59d497f26c5e865
Edilson Borges <edilsonfb@gmail.com>
2017-09-18 21:58:34 -0300

    Service manager

--- module/Blog/config/module.config.php
+++ module/Blog/config/module.config.php
@@ -2,6 +2,7 @@
  // Filename: /module/Blog/config/module.config.php
  return array(
      // This lines opens the configuration for the RouteManager
+     'service_manager' => array(),
      'view_manager' => [
              'template_path_stack' => [
                  __DIR__ . '/../view',
