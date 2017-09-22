b5f8bfc19bbda9ae1ce0a7733d316ef36a2ed0cb
Edilson Borges <edilsonfb@gmail.com>
2017-09-21 22:37:05 -0300

    Post Service Factory

---
+++ module/Blog/src/Blog/Factory/PostServiceFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Blog\Factory;
+
+use Blog\Service\PostService;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class PostServiceFactory implements FactoryInterface
+{
+
+}
