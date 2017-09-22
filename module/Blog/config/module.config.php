2d306df3e472e9d3699021de991121f1b17f99ba
Edilson Borges <edilsonfb@gmail.com>
2017-09-22 19:53:31 -0300

    Blog mapper factory

--- module/Blog/config/module.config.php
+++ module/Blog/config/module.config.php
@@ -12,6 +12,7 @@
      ),
      'service_manager' => array(
          'factories' => array(
+             'Blog\Mapper\PostMapperInterface'   => 'Blog\Factory\ZendDbSqlMapperFactory',
              'Blog\Service\PostServiceInterface' => 'Blog\Factory\PostServiceFactory',
              'Zend\Db\Adapter\Adapter'           => 'Zend\Db\Adapter\AdapterServiceFactory'
          )
