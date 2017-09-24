da1906d21cd43f759b7539c116331adba5539dd3
Edilson Borges <edilsonfb@gmail.com>
2017-09-24 16:46:00 -0300

    Create service function

--- module/Blog/src/Blog/Factory/ZendDbSqlMapperFactory.php
+++ module/Blog/src/Blog/Factory/ZendDbSqlMapperFactory.php
@@ -10,4 +10,12 @@ use Zend\Stdlib\Hydrator\ClassMethods;
 
 class ZendDbSqlMapperFactory implements FactoryInterface
 {
+  public function createService(ServiceLocatorInterface $serviceLocator)
+  {
+     return new ZendDbSqlMapper(
+         $serviceLocator->get('Zend\Db\Adapter\Adapter'),
+         new ClassMethods(false),
+         new Post()
+     );
+  }
 }
