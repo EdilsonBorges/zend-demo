8fc9ad6a6e43561432db4570ff20610d52d3d4f1
Edilson Borges <edilsonfb@gmail.com>
2017-09-24 16:45:35 -0300

    ZendDbSqlMapperFactory

---
+++ module/Blog/src/Blog/Factory/ZendDbSqlMapperFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Blog\Factory;
+
+use Blog\Mapper\ZendDbSqlMapper;
+use Blog\Model\Post;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Zend\Stdlib\Hydrator\ClassMethods;
+
+class ZendDbSqlMapperFactory implements FactoryInterface
+{
+}
