69163e2a13111cf09f24266926a19fa45c868542
Edilson Borges <edilsonfb@gmail.com>
2017-09-26 22:33:37 -0300

    new class

---
+++ module/Blog/src/Blog/Controller/WriteController.php
@@ -0,0 +1,12 @@
+<?php
+
+ namespace Blog\Controller;
+
+ use Blog\Service\PostServiceInterface;
+ use Zend\Form\FormInterface;
+ use Zend\Mvc\Controller\AbstractActionController;
+ use Zend\View\Model\ViewModel;
+
+ class WriteController extends AbstractActionController
+ {
+ }
